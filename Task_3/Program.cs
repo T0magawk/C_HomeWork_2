d330bfbf544548ab6f6cdaedd5f692dda1d38632
Александр Шушпанов <aleksandrshushpanov@MacBook-Air-Aleksandr.local>
2023-02-16 12:34:38 +0300

    Задача 3 готова

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -3,15 +3,18 @@
 // 7 -> да
 // 1 -> нет
 
-Console.WriteLine("Введите число от 1 до 7");
+Console.WriteLine("Введите число от 1 до 7, указывая день недели");
 int num = int.Parse(Console.ReadLine()!); 
-
+if (num > 7)
+{
+    Console.WriteLine("Неверный день недели");
+}
 if (num < 6)
 {
-    Console.WriteLine("Нет, рабочий");
+    Console.WriteLine("Нет, рабочий, иди работать, Раб");
 }
 
 if (num == 6 || num == 7)
 {
-    Console.WriteLine("Да, выходной");
+    Console.WriteLine("Да, выходной, Добби дали носок");
 }
